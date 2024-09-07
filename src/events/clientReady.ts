a2abdbd04caf0bab25a7c9e14c79e2b262582d5f
mraihanaf <mraihanaf0@gmail.com>
2024-09-07 20:24:27 +0700

    feat(events): load features on clientReady event

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,10 +1,12 @@
-import { Events, Client } from "discord.js";
+import { Events, Client } from "discord.js"
+import { loadFeatures } from "../handlers/features" 
 import log from "../utils/logger"
 
 export default {
     name: Events.ClientReady,
     once: true,
     execute: async (client: Client) => {
+        await loadFeatures(client)
         log.info(`Bot ${client.user?.tag} is online!`)
     }
 }
