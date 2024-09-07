952b3917a8ffbdc75edb7c24f1232045a2f35e97
mraihanaf <mraihanaf0@gmail.com>
2024-09-07 20:43:41 +0700

    feat(utils): add logger for each folders

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,4 +3,10 @@ const logger: Logger = pino({
     name: "EssaGalleryBot",
     level: process.env.LOG_LEVEL || "debug"
 })
+
+export const commandsLogger = logger.child({ module: "commands" })
+export const eventsLogger = logger.child({ module: "events"})
+export const featureLogger = logger.child({ module: "feature" })
+export const handlersLogger = logger.child({ module: "handlers"})
+
 export default logger
