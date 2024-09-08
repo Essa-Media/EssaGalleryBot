33df4f3d12ae7b98c1d760cffe96152d486f5e39
mraihanaf <mraihanaf0@gmail.com>
2024-09-08 19:45:26 +0700

    feat(gallery): update gallery types

--- src/types/gallery.d.ts
+++ src/types/gallery.d.ts
@@ -4,7 +4,9 @@ export type ChannelId = string
 export interface IGalleryMedia {
     id: string,
     type: "image"|"youtube",
-    url: string
+    url: string,
+    author: string, // discord author.tag
+    uploadDate: string  // this date format is ISO string (can be parsed with new Date())
 }
 
 export type IGallery = IGalleryMedia[]
